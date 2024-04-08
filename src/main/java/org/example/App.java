48b4047180680baeeaa0e48aa869e204eecf00f6
kushlenkov <kushlenkov.av@gmail.com>
2024-04-08 23:05:31 +0300

    After 6 lesson Hibernate

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import org.example.model.Item;
+import org.example.model.Person;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration()
+                .addAnnotatedClass(Person.class)
+                .addAnnotatedClass(Item.class);
+
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.getCurrentSession();
+
+        try {
+            session.beginTransaction();
+
+            Person person = session.get(Person.class, 4);
+            Item item = session.get(Item.class, 1);
+
+            item.getOwner().getItems().remove(item);
+
+//            SQL request
+            item.setOwner(person);
+
+            person.getItems().add(item);
+
+            session.getTransaction().commit();
+
+        } finally {
+            sessionFactory.close();
+        }
+    }
+}
