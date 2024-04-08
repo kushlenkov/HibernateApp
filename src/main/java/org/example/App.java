9c28ac5944de239de199a77058bd550d5450fb4e
kushlenkov <kushlenkov.av@gmail.com>
2024-04-08 23:37:35 +0300

    Merge commit

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,19 +1,24 @@
 package org.example;
 
-import org.example.model.Passport;
+import org.example.model.Item;
 import org.example.model.Person;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 /**
  * Hello world!
+ *
  */
 public class App {
     public static void main(String[] args) {
         Configuration configuration = new Configuration()
                 .addAnnotatedClass(Person.class)
-                .addAnnotatedClass(Passport.class);
+                .addAnnotatedClass(Item.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.getCurrentSession();
@@ -21,10 +26,18 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1 );
-            session.remove(person);
+            Person person = session.get(Person.class, 4);
+            Item item = session.get(Item.class, 1);
+
+            item.getOwner().getItems().remove(item);
+
+//            SQL request
+            item.setOwner(person);
+
+            person.getItems().add(item);
 
             session.getTransaction().commit();
+
         } finally {
             sessionFactory.close();
         }
